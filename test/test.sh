6d74bfc453b8ff87a150c17f628569ee6dc79587
Christian Uhsat <christian@uhsat.de>
2016-10-05 22:59:32 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ HOST=${1:-127.0.0.1}
 PORT=${2:-8211}
 
 main() {
-    echo "Testing on $HOST:$PORT ..."
+    echo "Starting on $HOST:$PORT ..."
     killall palantir 2> /dev/null || true
     ./palantir -d $HOST $PORT && \
     echo "-- halt" | ./palantir $HOST $PORT
