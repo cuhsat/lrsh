2da32739511d912427386c36ed0e4d4464e1d167
Christian Uhsat <christian@uhsat.de>
2016-10-09 19:11:37 +0200

    Version 1.8.4

--- src/palantir.h
+++ src/palantir.h
@@ -23,7 +23,7 @@
 
 #include <lua.h>
 
-#define VERSION "1.8.3"
+#define VERSION "1.8.4"
 
 #if LUA_VERSION_NUM < 503
 #error "Lua 5.3 required"
--- test/test.sh
+++ test/test.sh
@@ -5,11 +5,19 @@ set -o nounset
 HOST=${1:-127.0.0.1}
 PORT=${2:-8211}
 
+SERVER=~/.ci_palantird.pid
+CLIENT=~/.ci_palantir.pid
+
 main() {
-    killall palantir 2> /dev/null || true
-    ./palantir -d $HOST $PORT && \
-    echo "-- halt" | \
-    ./palantir $HOST $PORT
+    kill -9 $(cat $SERVER) 2> /dev/null || true
+    kill -9 $(cat $CLIENT) 2> /dev/null || true
+
+    ./palantir -d $HOST $PORT & echo $! > $SERVER
+    ./palantir $HOST $PORT & echo $! > $CLIENT
+
+    sleep 5
+
+    echo "-- halt" > /proc/$(cat $CLIENT)/fd/0
 }
 
 main "$@"
